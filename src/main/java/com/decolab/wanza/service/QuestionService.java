0fcd4e9ecf218ae49c5542fb0a0e6083010d7158
jjongta <75725170+jjongta@users.noreply.github.com>
2021-05-26 16:31:08 +0900

    question 몇개만들었는데 아직 애송이

---
+++ src/main/java/com/decolab/wanza/service/QuestionService.java
@@ -0,0 +1,23 @@
+package com.decolab.wanza.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.decolab.wanza.dao.QuestionDAO;
+import com.decolab.wanza.dto.QuestionDTO;
+
+@Service
+@Transactional
+public class QuestionService {
+	
+	@Autowired
+	QuestionDAO dao;
+	
+	public List<QuestionDTO> getAllQuestionList() {
+		return dao.getAllQuestionList();
+	}
+
+}
