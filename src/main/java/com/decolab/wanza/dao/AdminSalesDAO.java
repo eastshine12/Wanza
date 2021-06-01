fe427601cff005a5f5310bedc85bf08abf072cd7
janghs0282 <janghs0282@naver.com>
2021-06-01 20:08:34 +0900

    그래프 db연동

---
+++ src/main/java/com/decolab/wanza/dao/AdminSalesDAO.java
@@ -0,0 +1,17 @@
+package com.decolab.wanza.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import com.decolab.wanza.dto.admin.AdminSalesDTO;
+
+@Mapper
+@Repository
+public interface AdminSalesDAO {
+	
+	public List<AdminSalesDTO> graphList(AdminSalesDTO dto);
+	public List<AdminSalesDTO> mapList(AdminSalesDTO dto);
+
+}
