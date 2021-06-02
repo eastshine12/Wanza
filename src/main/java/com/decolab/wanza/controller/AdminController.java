56c93028674b537a50e7d9a75b3a4551620d0974
eastshine12 <eastshine_@naver.com>
2021-06-02 20:21:51 +0900

    210602 kdh

--- src/main/java/com/decolab/wanza/controller/AdminController.java
+++ src/main/java/com/decolab/wanza/controller/AdminController.java
@@ -1,16 +1,26 @@
 package com.decolab.wanza.controller;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.Date;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.decolab.wanza.dto.ProductDTO;
+import com.decolab.wanza.dto.ProductOptionDTO;
 import com.decolab.wanza.dto.admin.AdminStoryTagDTO;
 import com.decolab.wanza.service.AdminService;
+
+import util.NewFileName;
 
 @RestController
 public class AdminController {
@@ -65,8 +75,44 @@ public class AdminController {
 	}
 	
 	
+	@RequestMapping(value = "/writeProduct", method = {RequestMethod.GET,RequestMethod.POST})
+	public String writeProduct(@RequestParam("fileName")MultipartFile productFileName, ProductDTO pDto, HttpServletRequest req) {
+		System.out.println("AdminController writeProduct() " + new Date());	
+		/* 경로	src/main/webapp/upload 폴더를 생성할 것 */
+		String uploadPath = req.getServletContext().getRealPath("/upload");
+		//String uploadPath = "d:\\tmp";
+		
+		String filename = productFileName.getOriginalFilename();
+		
+		String newFilename = NewFileName.getNewFileName(filename);
+		String filepath = uploadPath + File.separator + newFilename;
+		
+		System.out.println("filepath :" + filepath);
+		pDto.setProductFileName(newFilename);
+
+		System.out.println(pDto.toString());
+		
+		try {
+			BufferedOutputStream os = new BufferedOutputStream(new FileOutputStream(new File(filepath)));
+			os.write(productFileName.getBytes());
+			os.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "file upload fail";
+		}
+		
+		return "suc";
+	}
 	
 	
+	@RequestMapping(value = "/writeProductOption", method = {RequestMethod.GET,RequestMethod.POST})
+	public String writeProductOption(ProductOptionDTO dto) {
+		System.out.println("AdminController writeProductOption() " + new Date());	
+		System.out.println(dto.toString());
+		
+		return "suc";
+	}
 	
 
 }
