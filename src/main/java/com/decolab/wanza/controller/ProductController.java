ca4736868d7e84b7f2ca52a9391a642a42e20aed
heo-min0 <humanzxc@naver.com>
2021-06-03 16:53:09 +0900

    product

--- src/main/java/com/decolab/wanza/controller/ProductController.java
+++ src/main/java/com/decolab/wanza/controller/ProductController.java
@@ -2,10 +2,12 @@ package com.decolab.wanza.controller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,5 +51,5 @@ public class ProductController {
 		System.out.println(dto.toString());
 		return service.getProductSortList(dto);
 	}
-	
+
 }
